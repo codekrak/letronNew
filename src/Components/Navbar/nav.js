7ee2351a2195b765d600b5ddfa4df2feb6d5a4d6
codekrak <codekrak68@gmail.com>
2022-05-06 23:25:04 +0000

    test push

---
+++ src/Components/Navbar/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hello() {
+    return (
+
+        <div className="navStart">
+            <h1>Hello there</h1>
+        </div>
+
+    );
+}
+
+export default Hello;
