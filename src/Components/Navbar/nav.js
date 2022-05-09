2a96d566e693a27491fc1abd31ef7bc654f8fa5b
codekrak <codekrak68@gmail.com>
2022-05-09 14:30:42 +0000

    updates on modal

--- src/Components/Navbar/nav.js
+++ src/Components/Navbar/nav.js
@@ -31,15 +31,32 @@ function Hello() {
             </div>
 
             {/* Modal Button */}
-            
-                <button id="modalBtn" className="button">Contact Us</button>
+            <div className="contactUs">
+                <button className="contact">Contact Us</button>
+                
+                <div classname="modal">
+                    <div classname="modal-dialog">
+                        <div classname="modal-content">
+                        
+                        <div classname="modal-header">
+                            <h5 classname="modal-title">Modal title</h5>
+                            <button type="button" classname="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        
+                        <div classname="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
 
-                <div id="simpleModal" className="modal">
-                    <div className="modal-content">
-                        <span className="closeBtn">&times;</span>
-                        <p>Hello...this is a modal</p>
+                        <div classname="modal-footer">
+                            <button type="button" classname="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" classname="btn btn-primary">Save changes</button>
+                        </div>
+
+                        </div>
                     </div>
                 </div>
+
+            </div>
             
 
             
